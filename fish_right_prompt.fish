6c09f6995ca9912a5ed4a111eca948b3e79d1ed9
Radek Podgorny <radek@podgorny.cz>
2015-10-31 22:52:36 +0100

    fish_right_prompt: format cmd_duration

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -2,7 +2,10 @@ function fish_right_prompt
 	set_color yellow
 
 	if set -q CMD_DURATION
-		echo "(+$CMD_DURATION)" (date "+%Y-%m-%d %H:%M:%S")
+		#echo "(+$CMD_DURATION)" (date "+%Y-%m-%d %H:%M:%S")
+		set secs (math "$CMD_DURATION / 1000")
+		set formatted (date -u -d @$secs "+%H:%M:%S")
+		echo "(+$formatted)" (date "+%Y-%m-%d %H:%M:%S")
 	else
 		date "+%Y-%m-%d %H:%M:%S"
 	end
