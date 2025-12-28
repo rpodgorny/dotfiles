dbddd72d430e8a8ab7429573b315a98a6606c06f
Radek Podgorny <radek@podgorny.cz>
2025-12-28 02:17:16 +0100

    nvim: automatically switch light/dark theme

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -9,6 +9,17 @@ return {
       vim.cmd('colorscheme vscode')
     end,
   },
+  {
+    'itsfernn/auto-gnome-theme.nvim',
+    lazy = false,  -- Load at startup to monitor system theme
+    priority = 999,  -- Load just before vscode theme (which has priority 1000)
+    dependencies = { 'Mofiqul/vscode.nvim' },  -- Ensure vscode theme is available
+    config = function()
+      require('auto-gnome-theme').setup({
+        theme = 'vscode'  -- Use vscode theme which has built-in light/dark variants
+      })
+    end,
+  },
   { 'RRethy/nvim-base16', lazy = true },
   { 'jschmold/sweet-dark.vim', lazy = true },
   { 'tjdevries/colorbuddy.vim', lazy = true },
