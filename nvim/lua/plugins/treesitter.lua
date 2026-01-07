3c01cc247e13e727ced600bb45cb6d2c4675da96
Radek Podgorny <radek@podgorny.cz>
2026-01-07 22:00:31 +0100

    nvim deps update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,9 +4,10 @@ return {
     build = ':TSUpdate',
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = "all",
-        ignore_install = { "ipkg" }, -- Skip problematic parsers
+      require('nvim-treesitter').setup({
+        ---ensure_installed = "all",
+        ensure_installed = { "lua", "vim", "vimdoc", "c", "rust", "python", "javascript", "typescript", "html", "css", "json" },
+        ---ignore_install = { "ipkg" }, -- Skip problematic parsers
         highlight = { enable = true },
       })
     end,
